61274f47d171f46e570ee94bfc5b8175ae066bda
Heather <heather@3iautoshot.com>
2022-04-26 16:15:48 -0400

    initial setup

--- src/pages/form/index.js
+++ src/pages/form/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './style.scss';
 import Names from "../../components/names";
 import Email from "../../components/email";
 import Message from '../../components/message';
@@ -6,11 +7,11 @@ import Message from '../../components/message';
 export default function Form() {
     return (
         <div>
-            <h1>Contact Us Form</h1>
+            <h1 className="header">Contact Us Form</h1>
             <Names />
             <Email />
             <Message />
-            <button>Submit</button>
+            <button className="submit-btn">Submit</button>
         </div>
     )
 }
