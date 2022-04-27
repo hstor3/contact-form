298c061941419e36c6fc6efea5689dd346b66ffb
Heather <heather@3iautoshot.com>
2022-04-27 18:40:29 -0400

    for to forHTML

--- src/components/message/index.js
+++ src/components/message/index.js
@@ -1,9 +1,10 @@
 import React from "react";
+import './style.scss';
 
 export default function Message() {
     return (
         <div>
-            <label for='message'>Message</label>
+            <label htmlFor='message'>Message</label>
             <textarea id="message" name="message" />
         </div>
     )
