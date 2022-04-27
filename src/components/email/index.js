b7a1ef9a5285ee08e1a22faee15d4f86d991dffd
Heather <heather@3iautoshot.com>
2022-04-27 18:39:55 -0400

    email component

--- src/components/email/index.js
+++ src/components/email/index.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Email() {
+    const [email, setEmail] = useState();
+
     return (
         <div>
-            <label for='email'>Email</label>
-            <input id="email" name="email" />
+            <label htmlFor='email'>Email</label>
+            <input id="email" name="email" type="email" defaultValue={email} onChange={(e) => setEmail(e.target.value)} />
         </div>
     )
 }
