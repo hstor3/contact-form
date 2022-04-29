0ac1548165ac9b3a7101a38ef3925e9d402f9e1e
Heather <heather@3iautoshot.com>
2022-04-28 22:06:23 -0400

    props reformatting

--- src/components/input/index.js
+++ src/components/input/index.js
@@ -1,14 +1,12 @@
 import React, { useState } from "react";
 
 export default function Input(props) {
-    const [input, setInput] = useState();
-    props.nameInput(input)
 
     return (
         <div>
             <div>
                 <label htmlFor={props.id}>{props.label}</label>
-                <input id={props.id} name={props.id} onInput={(e) => setInput(e.target.value)} />
+                <input type={props.type} id={props.id} name={props.id} onInput={(e) => props.input(e.target.value)} />
             </div>
         </div>
     )
--- src/components/message/index.js
+++ src/components/message/index.js
@@ -2,13 +2,11 @@ import React, { useState } from "react";
 import './style.scss';
 
 export default function Message(props) {
-    const [message, setMessage] = useState();
-    props.messageInput(message)
 
     return (
         <div>
             <label htmlFor='message'>Message</label>
-            <textarea id="message" name="message" onInput={(e) => setMessage(e.target.value)} />
+            <textarea id="message" name="message" onInput={(e) => props.messageInput(e.target.value)} />
         </div>
     )
 }
